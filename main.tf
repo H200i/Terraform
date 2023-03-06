992f70742d6d854914e93a2510e7483350cce46c
H200i <101483594+H200i@users.noreply.github.com>
2023-03-06 09:20:47 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -54,17 +54,6 @@ resource "aws_subnet" "my_subnet" {
   }
 }
 
-resource "aws_subnet" "my_subnet2" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "192.168.3.0/24"
-  availability_zone  = "us-east-1b"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "subnet2-ec2"
-  }
-}
-
 
 
 
